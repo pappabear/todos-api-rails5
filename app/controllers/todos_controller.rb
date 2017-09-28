5c22b2f138f8d645989013bc2376e11d47fc5b82
Chip Irek <chip.irek@gmail.com>
2017-09-28 06:33:16 -0500

    Initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,42 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  def index
+    render json: Todo.all
+  end
+
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      render json: {status: 500, err: 'todo could not be saved.'}
+    end
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(todo_params)
+    render json: todo
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    render json: {message: "successfully deleted!"}, status: 200
+  end
+        
+    def todo_params
+      # whitelist params
+      params.permit(:id, :title, :owner, :complete)
+    end
+            
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+  
+  end
